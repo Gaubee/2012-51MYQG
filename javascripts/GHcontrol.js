8dedc7d272a7e4981ecffc241218920a373be4f2
Gaubee <Gaubeebangeel@gmail.com>
2012-10-13 08:40:21 +0800

    New Ajax Framewrok

---
+++ javascripts/GHcontrol.js
@@ -0,0 +1,85 @@
+//封装GitHub.js的API转化成HTML
+var GH = window.GH = function () {
+    var _ = new Github({
+        username: "gaubeebangeel@gmail.com",
+        password: "gaubee546528",
+        auth: "basic"
+    });
+    var repo = _.getRepo("Gaubee", "2012-51MYQG");
+    var branch = 'gh-pages';
+
+    this.Repository = function () {
+        this.getList = function (PATH, Nece, Suffix) {
+            repo.getList(branch, PATH, function (err, list) {
+                console.log(list)
+                var html = "<ul>";
+                var length = list.length;
+                var er = [0, 0, 0];
+                for (var i = 0; i < length; i++) {
+                    var pathString = list[i].path;
+                    if (pathString.split("\/").length > 2) {
+                        //                        console.log('Too long');
+                        er[0]++;
+                        continue;
+                    }
+                    if (Nece) {
+                        if (Nece != "") {
+//                            console.log('No .html');
+                            if (pathString.search(Nece) == -1) {
+                                er[1]++;
+                                continue;
+                            }
+                        }
+                    }
+                    if (Suffix) {
+                        if (pathString.substr(-Suffix.length) != Suffix) {
+                            er[2]++;
+                            continue;
+                        }
+                    }
+                    html += "<li><a href=" + pathString + ">" + pathString + "</a></li>"
+                }
+                console.log(er[0] + "."+er[1]+"."+er[2])
+                html += "</ul>";
+                $("#md").html(html);
+            });
+        }
+        this.getRef = function (PATH) {
+            repo.getRef(PATH, function (err, sha) {
+                console.log(sha);
+            });
+        }
+        this.read = function (PATH) {
+            repo.read(branch, PATH, function (err, data) {
+                console.log(data);
+            });
+        }
+    } .call(this);
+};
+
+
+//alert(a.substring(0, 3)+',' + parseInt(a.substring(0, 2), 8).toString(16)+','+a.substring(3));
+
+//(function () {
+//    var E = "\"Notes/1.\\345\\210\\266\\344\\275\\234\\350\\276\\271\\346\\240\\217\\345\\260\\217\\345\\267\\245\\345\\205\\267\\347\\232\\204\\345\\237\\272\\346\\234\\254\\346\\212\\200\\345\\267\\247\\343\\200\\202.html\"";
+//    alert(E);
+//    var E_A = E.split("\\");
+//    var length = E_A.length;
+//    var it='';
+//    for (var i = 0; i < length; i++) {
+//        var S=parseInt(E_A[i],8).toString(16);
+//        S=="NaN"&&(it+=E_A[i])||(it+="%"+S);
+//    }
+//    alert(it);
+//    alert(decodeURIComponent(it));
+//    //alert(E.replace(new RegExp("\\", "gm"), "%"));
+//})();
+
+//alert('a')&&function(){alert('b');};
+
+//window.tree = [];
+//var length = N.tree.length;
+//for (var i = 0; i < length; i++) {
+//var content=N.tree[i];
+//content["type"] === "blob" && content["path"].search('\"')==-1 && tree.push(N.tree[i]["path"])
+//}
--- javascripts/main.js
+++ javascripts/main.js
@@ -1,1 +1,88 @@
-console.log('This would be the main JS file.');
+
+getAllAttrs = function (obj) {
+    var it = '';
+    for (var i in obj) {
+        it += i + ':' + obj[i] + '\n';
+    }
+    alert(it);
+};
+$(function ($) {
+    window.github = new GH();
+    window.tagsTIME = false; //控制防止动画时多次点击造成动画错乱
+    $("#tags .tag").live({
+        mouseover: function () {
+            $(this).animate({ height: '46px' }, 200);
+            $(this).children("h2").animate({ marginTop: '2px' }, 200);
+        },
+        mouseout: function () {
+            $(this).animate({ height: '30px' }, 200);
+            $(this).children("h2").animate({ marginTop: '-6px' }, 200);
+        },
+        click: function () {
+            if (tagsTIME) {
+                return;
+            }
+            tagsTIME = true;
+            var tagsa = $("#tags a"), i = 0;
+            var length = tagsa.length;
+            for (; i < length; i++) {
+                if (tagsa[i] == this) {
+                    break;
+                }
+            }
+            $(tagsa[0]).animate({ marginRight: '96px' }, 200).animate({ marginTop: '31px', width: '90px' }, 200).addClass("tag").removeClass("tagvisiting");
+            $(tagsa[i]).animate({ marginRight: '-96px' }, 200); // (96 * (j + 1) + 'px') 因相对移动，后面的元素也发生了移动，即使没有给改变数值
+            $(this).animate({ marginRight: (-96 * (i + 1) + 'px') }, 200 * i);
+            $(this).addClass("TODO").off();
+            github.getList($(this).attr("href").replace("#", ""), ".html");
+            setTimeout(function () {
+                $("#tags a").attr('style', '').find('h2').attr('style', '');
+                var TODO = $('.TODO').addClass("tagvisiting").removeClass("tag TODO");
+                $("#tags").prepend(TODO);
+
+                tagsTIME = false;
+            }, i * 200 + 700);
+
+        }
+    });
+
+
+    //   window.N= github.getList("Notes");
+
+
+    var converter = new Showdown.converter();
+    loadMD = function () {
+        $.ajax({
+            url: "index.md",
+            cache: false,
+            dataType: "text",
+            success: function (data, type) {
+                $("#md").html(converter.makeHtml(data));
+            }
+        });
+    }
+    ajax_get = function () {
+        $.ajax({
+            url: ($("#ajax_get").attr("value")),
+            dataType: "text",
+            data: ($("#ajax_data").attr("value")),
+            success: function (data, type) {
+                getAllAttrs(data);
+            }
+        });
+    }
+    ajax_post = function () {
+        $.ajax({
+            url: ($("#ajax_post").attr("value")),
+            dataType: "text",
+            data: ($("#ajax_data").attr("value")),
+            type: "POST",
+            success: function (data, type) {
+                getAllAttrs(data);
+            }
+        });
+    }
+    //    $("#md").html(converter.makeHtml($("#md").text()));
+
+
+})();
